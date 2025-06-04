dfc9b02c134e56ce275a87fe7c6810fedc3c5630
Ikhsan <ikhsan.budi.wicaksono@students.untidar.ac.id>
2025-06-04 08:25:18 +0700

    Fragment Add
    Adding every fragment for the page

--- app/src/main/java/com/example/moveandclean/HomeFragment.java
+++ app/src/main/java/com/example/moveandclean/HomeFragment.java
@@ -1,17 +1,64 @@
 package com.example.moveandclean;
-// app/src/main/java/com/yourpackage/HomeFragment.java
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-// Add other imports as needed
 
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class HomeFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private RecyclerView rvPopularServices;
+    private RecyclerView rvProviders;
 
     public HomeFragment() {
         // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment HomeFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static HomeFragment newInstance(String param1, String param2) {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
     @Override
@@ -25,22 +72,14 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // Initialize your UI elements here, using the 'view' parameter
-        // 'view' is the root view of fragment_home.xml
-        etSearch = view.findViewById(R.id.et_search);
         rvPopularServices = view.findViewById(R.id.rv_popular_services);
         rvProviders = view.findViewById(R.id.rv_providers);
-        imgPromoBanner = view.findViewById(R.id.img_promo_banner);
 
-        // Now you can set up your RecyclerViews, click listeners for etSearch, etc.
-        // Example for RecyclerViews:
-        if (rvPopularServices != null) {
-            rvPopularServices.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
-            // rvPopularServices.setAdapter(new PopularServicesAdapter(...)); // Set your adapter here
-        }
-        if (rvProviders != null) {
-            rvProviders.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
-            // rvProviders.setAdapter(new ProvidersAdapter(...)); // Set your adapter here
-        }
+        rvPopularServices.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        rvProviders.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        // TODO: Set adapters here
+        // rvPopularServices.setAdapter(new PopularServicesAdapter(...));
+        // rvProviders.setAdapter(new ProvidersAdapter(...));
     }
 }
